2fbf8862fd1d59c85282f9952b30f1c92f10ccd3
Dragon <mkmaster@gmail.com>
2021-04-14 08:40:19 +0300

    Update offsets and signatures for 5.5

--- Visibility/Structs/Client/Game/Character/Character.cs
+++ Visibility/Structs/Client/Game/Character/Character.cs
@@ -12,10 +12,10 @@ namespace Visibility.Structs.Client.Game.Character
 		[FieldOffset(ActorOffsets.BattleNpcTargetActorId)] public uint BattleNpcTargetActorId;
 		[FieldOffset(ActorOffsets.CompanyTag)] public fixed byte CompanyTag[7];
 		[FieldOffset(ActorOffsets.NameId)] public int NameID;
-		[FieldOffset(0x1930)] public uint CompanionOwnerID;
+		[FieldOffset(0x1950)] public uint CompanionOwnerID;
 		[FieldOffset(ActorOffsets.CurrentWorld)] public ushort CurrentWorld;
 		[FieldOffset(ActorOffsets.HomeWorld)] public ushort HomeWorld;
 		[FieldOffset(ActorOffsets.CurrentHp)] public int CurrentHp;
-		[FieldOffset(0x1980)] public byte StatusFlags;
+		[FieldOffset(0x19A0)] public byte StatusFlags;
 	}
 }
